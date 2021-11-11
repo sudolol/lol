bfa570660ff57c32abac62ee054466f59ee93db7
Cl1cker <75802182+Cl1ckerr@users.noreply.github.com>
2021-11-11 19:41:07 -0400

    Add files via upload

---
+++ js/randomvid.js
@@ -0,0 +1,199 @@
+const urlParams = new URLSearchParams(window.location.search);
+const songId = urlParams.get('s');
+
+const videoSettings = '?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&enablejsapi=1&amp;';
+let video = new Array();
+
+// Virginity Syndrome
+video.push({
+	video_id: 'm2K7Bn4ApsE',
+	video_name: 'Virginity Syndrome',
+	id: 'm2K7Bn4ApsE' + videoSettings + 'start=0',
+	note: 'Mai Sakurajima ❤.',
+	volume: 35,
+}); 
+// Wombo Combo
+video.push({
+	video_id: 'X9zaArvqZXM',
+	video_name: 'Wombo Combo',
+	id: 'X9zaArvqZXM' + videoSettings + 'start=0',
+	note: 'Thats a Motto.',
+	volume: 35,
+}); 
+// MC Virgins - Virgins Club
+video.push({
+	video_id: 'Zmy6U2sF6Co',
+	video_name: 'Virgins Club',
+	id: 'Zmy6U2sF6Co' + videoSettings + 'start=0',
+	note: 'Sex is really gross, but I wanna have it.',
+	volume: 35,
+}); 
+// Yung Nugget & MC Virgins - Stay Strapped
+video.push({
+	video_id: 'i6_Rm24yzgc',
+	video_name: 'Stay Strapped',
+	id: 'i6_Rm24yzgc' + videoSettings + 'start=0',
+	note: 'I forgot to stay strapped.',
+	volume: 35,
+}); 
+// Shotgun Willy x Yung Craka - Breakfast in Heaven
+video.push({
+	video_id: 'dSX55V3HnEc',
+	video_name: 'Breakfast in Heaven',
+	id: 'dSX55V3HnEc' + videoSettings + 'start=0',
+	note: 'I think i love this bitch cuz her pussy feels like heaven.',
+	volume: 35,
+}); 
+// Shotgun Willy- Cheat Codes for Hoes (ft.Traqula)
+video.push({
+	video_id: '32DOaHV7N1U',
+	video_name: 'Cheat Codes for Hoes',
+	id: '32DOaHV7N1U' + videoSettings + 'start=0',
+	note: 'I been flippin switches cause i know i can.',
+	volume: 35,
+}); 
+// Shotgun Willy - Just Got Sued
+video.push({
+	video_id: '07_aE3vp6bQ',
+	video_name: 'Just Got Sued',
+	id: '07_aE3vp6bQ' + videoSettings + 'start=0',
+	note: 'You betta respect the grind.',
+	volume: 35,
+}); 
+// 1nonly - Bunny Girl (Feat. Ciscaux)
+video.push({
+	video_id: 'pKBLxngfylA',
+	video_name: 'Bunny Girl',
+	id: 'pKBLxngfylA' + videoSettings + 'start=0',
+	note: 'That lil&apos; bitch a goth girl.',
+	volume: 35,
+}); 
+// Shotgun Willy x Yung Craka - Blues Clues (Prod. Lucian x ANH)
+video.push({
+	video_id: 'XG2x9eV846M',
+	video_name: 'Blues Clues',
+	id: 'XG2x9eV846M' + videoSettings + 'start=0',
+	note: 'Call yo bitch up on my flip phone.',
+	volume: 35,
+}); 
+// Shakira!
+video.push({
+	video_id: 'AQ5ZRB9oWRw',
+	video_name: 'Shakira!',
+	id: 'AQ5ZRB9oWRw' + videoSettings + 'start=0',
+	note: 'She fuck with me the most, like damn.',
+	volume: 35,
+}); 	
+// 1nonly x lilbubblegum - ash kaashh
+video.push({
+	video_id: 'qkfPR__g3oc',
+	video_name: 'Ash Kaashh',
+	id: 'qkfPR__g3oc' + videoSettings + 'start=0',
+	note: 'Rolex that&apos;s drip.',
+	volume: 35,
+}); 	
+// goth girl - basco
+video.push({
+	video_id: 'IvFwz6amBaM',
+	video_name: 'Goth Girl',
+	id: 'IvFwz6amBaM' + videoSettings + 'start=0',
+	note: 'I got myself a goth girl.',
+	volume: 35,
+}); 	
+// MC Virgins & MOL$ - MINUTE MAID!
+video.push({
+	video_id: '0lTfcsaShE4',
+	video_name: 'Minute Maid!',
+	id: '0lTfcsaShE4' + videoSettings + 'start=0',
+	note: 'Sippin on that Minute Maid.',
+	volume: 35,
+}); 
+// MC Virgins & Yun Head - Trap Anthem
+video.push({
+	video_id: 'Oxr7OQKoY8Q',
+	video_name: 'Trap Anthem',
+	id: 'Oxr7OQKoY8Q' + videoSettings + 'start=0',
+	note: 'She sound like...',
+	volume: 35,
+}); 
+// Shotgun Willy - Last Chance (Prod. CoridaArtist)
+video.push({
+	video_id: 'm0nTcyNDmRU',
+	video_name: 'Last Chance',
+	id: 'm0nTcyNDmRU' + videoSettings + 'start=0',
+	note: 'Sorry baby but im married to the money.',
+	volume: 35,
+}); 
+// Shotgun Willy - Captain Save a Hoe
+video.push({
+	video_id: 'mJ8vi-kIlqE',
+	video_name: 'Captain Save a Hoe',
+	id: 'mJ8vi-kIlqE' + videoSettings + 'start=0',
+	note: 'Send a new DM like every hour.',
+	volume: 35,
+}); 
+// Oh Klahoma - Jack Stauber
+video.push({
+	video_id: 'c7flzLTOD5I',
+	video_name: 'Oh Klahoma ',
+	id: 'c7flzLTOD5I' + videoSettings + 'start=0',
+	note: 'Saddest little baby in the room.',
+	volume: 35,
+}); 
+// Baby Hotline - Jack Stauber
+video.push({
+	video_id: 'CY05nUQ4D5M',
+	video_name: 'Baby Hotline',
+	id: 'CY05nUQ4D5M' + videoSettings + 'start=0',
+	note: 'Maybe snack time?.',
+	volume: 35,
+}); 
+// Caught Simpin (Prod. by Fumiharo)
+video.push({
+	video_id: 'Ljnkns6k0Q4',
+	video_name: 'Caught Simpin',
+	id: 'Ljnkns6k0Q4' + videoSettings + 'start=0',
+	note: 'Open up your mind, Don&apos;t open up your thighs.',
+	volume: 35,
+}); 
+// Stay With Me - 1nonly
+video.push({
+	video_id: 'ETecZsoA0jo',
+	video_name: 'Stay With Me',
+	id: 'ETecZsoA0jo' + videoSettings + 'start=0',
+	note: 'You be cappin&apos; like a snitch, no thots won&apos;t stand.',
+	volume: 35,
+}); 
+// Pretty Girl - Clairo
+video.push({
+	video_id: 'mngtcfcaVrI',
+	video_name: 'Pretty Girl',
+	id: 'mngtcfcaVrI' + videoSettings + 'start=0',
+	note: 'I&apos;ll wear a skirt for you <3.',
+	volume: 35,
+}); 
+// DVRST - CLOSE EYES
+video.push({
+	video_id: 'ao4RCon11eY',
+	video_name: 'Close Eyes',
+	id: 'ao4RCon11eY' + videoSettings + 'start=0',
+	note: 'Megamind go brr.',
+	volume: 35,
+}); 
+// Shotgun Willy - Wendy (Ft. Yung Craka)
+video.push({
+	video_id: 'GPNMBxK97fE',
+	video_name: 'Wendy',
+	id: 'GPNMBxK97fE' + videoSettings + 'start=0',
+	note: 'I&apos;m making a brick, I got me a chick, she drivin&apos; me crazy.',
+	volume: 35,
+}); 
+// Shotgun Willy - So What? (Prod. Joji)
+video.push({
+	video_id: 'udDND0Avznc',
+	video_name: 'So What?',
+	id: 'udDND0Avznc' + videoSettings + 'start=0',
+	note: 'Shorty got a man i don’t care, So What.',
+	volume: 35,
+}); 
+let randVid = songId && songId >= 0 && songId < video.length ? songId : Math.floor(video.length * Math.random());
